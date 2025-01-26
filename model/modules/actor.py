Tanh squashing and numerical stability fixes

model/modules/actor.py
@@ -1,3 +1,4 @@
+import torch
 from torch import nn
 from torch.distributions import Normal
 
@@ -9,9 +10,12 @@ class Actor(nn.Module):
         state_space: spaces.Space,
         action_space: spaces.Box,
         num_layers: int,
-        hidden_dim: int
+        hidden_dim: int,
+        epsilon: float = 1e-6
     ):
         super(Actor, self).__init__()
+
+        self.epsilon = epsilon
 
         # Observation -> Latent State Space
         layers = [
@@ -44,6 +48,12 @@ class Actor(nn.Module):
 
         return z, log_prob
 
+    def _squash(self, action, log_prob):
+        action = torch.tanh(action)
+        # Squash Correction from original paper
+        log_prob -= torch.sum(torch.log(1 - action**2 + self.epsilon), dim=1)
+        return action, log_prob
+
     def forward(self, x, deterministic=False):
         latent = self.actor_latent(x)
 
@@ -52,7 +62,12 @@ class Actor(nn.Module):
         if deterministic:
             return mu
 
-        log_std = self.actor_log_std(latent)
+        log_std = self.actor_log_std(latent).clamp(min=-20, max=2)
 
-        return self._sample(mu, log_std)
+        action, log_prob = self._sample(mu, log_std)
+
+        # Squash to [-1, 1]
+        action, log_prob = self._squash(action, log_prob)
+
+        return action, log_prob
 
20dda15fc386a4e57ced0ab1a975a213887110bf
Erik Ayari <erik.ayari22@gmail.com>
2025-02-14 19:42:18 +0100

    Tournament Client

---
+++ tournament/run_client.py
@@ -0,0 +1,130 @@
+from __future__ import annotations
+
+import argparse
+import uuid
+
+import hockey.hockey_env as h_env
+import numpy as np
+
+
+from comprl.client import Agent, launch_client
+
+import torch
+from model.modules import Actor
+
+class SACAgent(Agent):
+    """A hockey agent that simply uses random actions."""
+    def __init__(self, load_path : str):
+        super().__init__()
+        checkpoint = torch.load(load_path, map_location="cpu")
+        state_dict = checkpoint["state_dict"]
+        self.actor = Actor(state_dim=18, action_dim=4, num_layers=1, hidden_dim=256)
+        actor_state_dict = {k.replace("actor.", ""): v for k, v in state_dict.items() if k.startswith("actor.")}
+        self.actor.load_state_dict(actor_state_dict)
+
+    def get_step(self, observation: list[float]) -> list[float]:
+        observation = torch.tensor(observation).unsqueeze(0)
+        return self.actor(observation, deterministic=True).flatten().tolist()
+
+    def on_start_game(self, game_id) -> None:
+        print("game started")
+
+    def on_end_game(self, result: bool, stats: list[float]) -> None:
+        text_result = "won" if result else "lost"
+        print(
+            f"game ended: {text_result} with my score: "
+            f"{stats[0]} against the opponent with score: {stats[1]}"
+        )
+
+class RandomAgent(Agent):
+    """A hockey agent that simply uses random actions."""
+
+    def get_step(self, observation: list[float]) -> list[float]:
+        return np.random.uniform(-1, 1, 4).tolist()
+
+    def on_start_game(self, game_id) -> None:
+        print("game started")
+
+    def on_end_game(self, result: bool, stats: list[float]) -> None:
+        text_result = "won" if result else "lost"
+        print(
+            f"game ended: {text_result} with my score: "
+            f"{stats[0]} against the opponent with score: {stats[1]}"
+        )
+
+
+class HockeyAgent(Agent):
+    """A hockey agent that can be weak or strong."""
+
+    def __init__(self, weak: bool) -> None:
+        super().__init__()
+
+        self.hockey_agent = h_env.BasicOpponent(weak=weak)
+
+    def get_step(self, observation: list[float]) -> list[float]:
+        # NOTE: If your agent is using discrete actions (0-7), you can use
+        # HockeyEnv.discrete_to_continous_action to convert the action:
+        #
+        # from hockey.hockey_env import HockeyEnv
+        # env = HockeyEnv()
+        # continuous_action = env.discrete_to_continous_action(discrete_action)
+
+        action = self.hockey_agent.act(observation).tolist()
+        return action
+
+    def on_start_game(self, game_id) -> None:
+        game_id = uuid.UUID(int=int.from_bytes(game_id))
+        print(f"Game started (id: {game_id})")
+
+    def on_end_game(self, result: bool, stats: list[float]) -> None:
+        text_result = "won" if result else "lost"
+        print(
+            f"Game ended: {text_result} with my score: "
+            f"{stats[0]} against the opponent with score: {stats[1]}"
+        )
+
+
+# Function to initialize the agent.  This function is used with `launch_client` below,
+# to lauch the client and connect to the server.
+def initialize_agent(agent_args: list[str]) -> Agent:
+    # Use argparse to parse the arguments given in `agent_args`.
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--agent",
+        type=str,
+        choices=["weak", "strong", "random", "sac"],
+        default="weak",
+        help="Which agent to use.",
+    )
+    parser.add_argument(
+        "--load_path",
+        "-p",
+        type=str,
+        default="logs/sac_hockey_strong/version_1/checkpoints/epoch=1499999-step=4500000.ckpt",
+        help="Checkpoint file to load"
+    )
+    args = parser.parse_args(agent_args)
+
+    # Initialize the agent based on the arguments.
+    agent: Agent
+    if args.agent == "weak":
+        agent = HockeyAgent(weak=True)
+    elif args.agent == "strong":
+        agent = HockeyAgent(weak=False)
+    elif args.agent == "random":
+        agent = RandomAgent()
+    elif args.agent == "sac":
+        agent = SACAgent(args.load_path)
+    else:
+        raise ValueError(f"Unknown agent: {args.agent}")
+
+    # And finally return the agent.
+    return agent
+
+
+def main() -> None:
+    launch_client(initialize_agent)
+
+
+if __name__ == "__main__":
+    main()

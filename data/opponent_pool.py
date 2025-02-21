e8bb5bd6131a7b0ff94e8cc3229685a19d4d2af3
Erik Ayari <erik.ayari22@gmail.com>
2025-02-21 12:38:18 +0100

    OpponentPool can now handle foreign agents

--- data/opponent_pool.py
+++ data/opponent_pool.py
@@ -1,6 +1,7 @@
 import random
 import torch
 import trueskill
+import numpy as np
 
 from hockey.hockey_env import BasicOpponent
 from model.modules import Actor
@@ -9,17 +10,26 @@ class OpponentPool():
     def __init__(
         self,
         pool_size: int,
-        actor_params: dict
+        actor_params: dict,
+        foreign_agents = None
     ):
         self.basic_weak     = BasicOpponent(weak=True)
         self.basic_strong   = BasicOpponent(weak=False)
+
+        self.foreign_agents = foreign_agents
+        self.num_foreign_agents = 0 if self.foreign_agents == None else len(self.foreign_agents)
+
         self.snapshots      = []
+
         self.pool_size      = pool_size
         self.actor_params   = actor_params
 
         self.ratings = []
         self.ratings.append(trueskill.Rating())
         self.ratings.append(trueskill.Rating())
+        for _ in range(self.num_foreign_agents):
+            self.ratings.append(trueskill.Rating())
+        self.snapshot_start_idx = 2 + self.num_foreign_agents
 
     def add_snapshot(self, actor):
         snapshot = Actor(
@@ -38,10 +48,10 @@ class OpponentPool():
 
         if len(self.snapshots) > self.pool_size:
             self.snapshots.pop(0)
-            self.ratings.pop(2)
+            self.ratings.pop(self.snapshot_start_idx)
     
     def sample_opponent(self):
-        self.opponent_idx = random.randint(0, len(self.snapshots) + 1)
+        self.opponent_idx = random.randint(0, len(self.snapshots) + self.num_foreign_agents + 1)
 
     def set_opponent(self, idx):
         self.opponent_idx = idx
@@ -51,6 +61,9 @@ class OpponentPool():
             return self.basic_weak.act(observation)
         elif self.opponent_idx == 1:
             return self.basic_strong.act(observation)
+        elif self.opponent_idx < self.snapshot_start_idx:
+            observation = torch.tensor(observation, dtype=torch.float).unsqueeze(0)
+            return np.array(self.foreign_agents[self.opponent_idx - 2].act(observation))
         else:
             with torch.no_grad():
                 observation = torch.tensor(observation, dtype=torch.float).unsqueeze(0)
@@ -71,4 +84,4 @@ class OpponentPool():
         return self.ratings[opponent_idx].mu
 
     def __len__(self):
-        return len(self.snapshots) + 2
+        return len(self.snapshots) + self.num_foreign_agents + 2
